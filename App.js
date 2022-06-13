0656f35bf597f4ef0891e9e41ff3b7ad0f75b363
Elias <eliasmanica@poli.ufrj.br>
2022-06-13 17:05:33 -0300

    feita as rotas da tab bar

--- App.js
+++ App.js
@@ -4,12 +4,6 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 
-import Button from "./src/components/Button";
-import Input from "./src/components/Input";
-
-import { screenHeight, screenWidth } from "./src/constants/dimensions";
-import colors from "./src/constants/colors";
-
 import LoginScrren from "./src/screens/loggedOut/LoginScrren";
 import SingUpScreen from "./src/screens/loggedOut/SingUpScreen";
 import ForgetPasswordScreen from "./src/screens/loggedOut/ForgetPassword";
@@ -22,13 +16,14 @@ import ConteudosScreen from "./src/screens/loggedIn/ConteudosScreen";
 import ContactScreen from "./src/screens/loggedIn/ContactScreen";
 
 import SignedOutRoutes from "./src/routes/signedOut.routes";
+import SignedInRoutes from "./src/routes/signedIn.routes";
 
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <ContactScreen />
+      <SignedInRoutes />
       <StatusBar style="auto" />
     </NavigationContainer>
   );
--- src/routes/signedIn.routes.js
+++ src/routes/signedIn.routes.js
@@ -6,6 +6,7 @@ import HomeScreen from "../screens/loggedIn/HomeScreen";
 import SistemaWebScreen from "../screens/loggedIn/SistemaWebScreen";
 import WhoWeAreScreen from "../screens/loggedIn/WhoWeAreScreen";
 import ConteudosScreen from "../screens/loggedIn/ConteudosScreen";
+import ContactScreen from "../screens/loggedIn/ContactScreen";
 
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -17,6 +18,9 @@ export default function SignedInRoutes() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="WhoAre" component={WhoWeAreScreen} />
+      <Tab.Screen name="ConteudosScreen" component={ConteudosScreen} />
+      <Tab.Screen name="ContactScreen" component={ContactScreen} />
     </Tab.Navigator>
   );
 }
