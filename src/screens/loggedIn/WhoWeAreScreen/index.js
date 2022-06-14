fc60fb3d464500e5c64d6bdb5fe64fe02f8d1b45
Elias <eliasmanica@poli.ufrj.br>
2022-06-14 00:39:15 -0300

    confeccionando tela do quem somos

--- src/screens/loggedIn/WhoWeAreScreen/index.js
+++ src/screens/loggedIn/WhoWeAreScreen/index.js
@@ -1,12 +1,43 @@
 import React from "react";
 import { View } from "react-native";
 
-import { Container, TituloProjetos } from "./styles";
+import {
+  Container,
+  TituloProjetos,
+  ImagemDiretoria,
+  TextToReformedPass,
+  Caixa,
+} from "./styles";
+
+import diretoria from "../../../assets/diretoria.png";
+import fluxoLogo from "../../../assets/fluxoLogo.png";
 
 const WhoWeAreScreen = () => {
   return (
     <Container>
-      <TituloProjetos>WhoWeAreScreen</TituloProjetos>
+      <TituloProjetos>
+        A Fluxo Consultoria é uma empresa sem fins lucrativos, que realiza
+        projetos em diversas áreas da Engenharia, além de projetos de
+        Arquitetura. Abrangemos todos os cursos da Escola Politécnica, da Escola
+        de Química e da Faculdade de Arquitetura e Urbanismo da UFRJ, uma das
+        maiores instituições de ensino do país. Todo nosso trabalho tem a sua
+        supervisão, bem como mentorias de grandes empresas do mercado.
+      </TituloProjetos>
+      <ImagemDiretoria source={diretoria} />
+      <TextToReformedPass>
+        Nossa Missão Promover acesso ao conhecimento, impactando o Brasil com
+        projetos de engenharia.
+      </TextToReformedPass>
+      <TextToReformedPass>
+        Nossa Visão Sermos a Empresa Júnior referência em entregar soluções
+        aplicáveis, garantindo o sucesso dos nossos clientes através de projetos
+        multidisciplinares.
+      </TextToReformedPass>
+      <TextToReformedPass>
+        Nossos Valores Atitude de Dono, Sentimento de Time, Honestidade e
+        Transparência, Humildade e Excedência, Foco no Cliente, Sede por
+        Desenvolvimento e Orgulho de ser Fluxo.
+      </TextToReformedPass>
     </Container>
   );
 };
--- src/screens/loggedIn/WhoWeAreScreen/styles.js
+++ src/screens/loggedIn/WhoWeAreScreen/styles.js
@@ -9,12 +9,34 @@ export const Container = styled.ScrollView`
 `;
 
 export const TituloProjetos = styled.Text`
-  width: 300px;
-  height: 27px;
+  width: 90%;
   font-weight: 900;
   font-size: 15px;
   line-height: 18px;
-  margin-top: ${screenHeight * 0.04}px;
-  margin-left: ${screenWidth * 0.04}px;
-  color: ${colors.colors};
+  margin-top: ${screenHeight * 0.03}px;
+  margin-left: auto;
+  margin-right: auto;
+  color: ${colors.preto};
+  text-align: center;
+  display: flex;
+`;
+
+export const ImagemDiretoria = styled.Image`
+  margin-top: ${screenHeight * 0.03}px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const TextToReformedPass = styled.Text`
+  font-size: 15px;
+  font-weight: 400;
+  color: ${colors.background};
+  margin-top: ${screenHeight * 0.02}px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Caixa = styled.View`
+  width: ${screenWidth * 0.34}px;
+  height: ${screenHeight * 0.3}px;
 `;
